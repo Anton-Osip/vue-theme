9880acae647e3306dc1d0f94367a7a4b2eedb476
stas <kepamuk@yandex.ru>
2025-11-05 17:50:58 +0300

    v-model

--- 05-forms/007_1/src/App.vue
+++ 05-forms/007_1/src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="container">
-    <form class="card">
+    <form class="card" @submit.prevent="submitHandler">
       <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control">
-        <label for="name">Как тебя зовут?</label>
-        <input type="text" id="name" placeholder="Введи имя">
-      </div>
+
+      <app-input
+          v-model.trim="name"
+          :error="errors.name"
+          label="Как тебя зовут?"
+          placeholder="Введи имя"
+      ></app-input>
 
       <div class="form-control">
         <label for="age">Выбери возраст</label>
-        <input type="number" id="age" value="20">
+        <input
+            type="number"
+            id="age"
+            value="20"
+            v-model.number="age"
+        >
       </div>
 
       <div class="form-control">
         <label for="city">Твой город</label>
-        <select id="city">
+        <select id="city" v-model="city">
           <option value="spb">Санкт-Петербург</option>
           <option value="msk">Москва</option>
           <option value="kzn">Казань</option>
@@ -25,24 +33,24 @@
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Да</label>
+          <label><input type="radio" name="trip" v-model="relocate" value="yes"/> Да</label>
         </div>
 
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Нет</label>
+          <label><input type="radio" name="trip" v-model="relocate" value="no"/> Нет</label>
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vuex</label>
+          <label><input type="checkbox" v-model="skills" value="Vuex" name="skills"/> Vuex</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue CLI</label>
+          <label><input type="checkbox" v-model="skills" value="CLI" name="skills"/> Vue CLI</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue Router</label>
+          <label><input type="checkbox" v-model="skills" value="Router" name="skills"/> Vue Router</label>
         </div>
       </div>
 
@@ -52,9 +60,56 @@
 </template>
 
 <script>
-  export default {}
+import AppInput from "@/AppInput.vue";
+
+export default {
+  components: {AppInput},
+  data: () => ({
+    name: '',
+    age: 23,
+    city: 'nsk',
+    relocate: null,
+    skills: [],
+    errors: {
+      name: null
+    }
+  }),
+  methods: {
+    formIsValid() {
+      let isValid = true
+
+      if (this.name.length === 0) {
+        isValid = false
+        this.errors.name = 'name is required'
+      } else {
+        this.errors.name = null
+      }
+
+      return isValid
+    },
+    submitHandler() {
+      if (this.formIsValid()) {
+        console.group('form data')
+        console.log('name', this.name)
+        console.log('age', this.age)
+        console.log('city', this.city)
+        console.log('relocate', this.relocate)
+        console.log('skills', this.skills)
+        console.groupEnd()
+      }
+    }
+  },
+  computed: {AppInput}
+}
 </script>
 
 <style>
+.form-control small {
+  color: red;
+}
 
+.form-control.invalid input {
+  border: 1px solid red;
+
+}
 </style>
---
+++ 05-forms/007_1/src/AppInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="form-control" :class="{'invalid':error}">
+    <label :for="id">{{ label }}</label>
+    <input
+        :type="type"
+        :id="id"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="change"
+    >
+    <small v-if="error">{{ error }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['update:modelValue'],
+  props: {
+    modelValue: String,
+    label: String,
+    error: String,
+    placeholder: String,
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    }
+  },
+  methods: {
+    change(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  },
+  data: () => ({
+    id: 'input' + Math.random().toString(36)
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
