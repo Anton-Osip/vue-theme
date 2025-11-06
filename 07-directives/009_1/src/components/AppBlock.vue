0831cf2aa5562f92812d8745a07d9c672b3b88aa
stas <kepamuk@yandex.ru>
2025-11-06 15:57:26 +0300

    mixin

--- 07-directives/009_1/src/components/AppBlock.vue
+++ 07-directives/009_1/src/components/AppBlock.vue
@@ -2,30 +2,26 @@
   <div class="container">
     <app-alert
         v-if="alert"
-        type="warning"
+        type="primary"
         title="Внимание!"
         text="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Alias animi asperiores at cum, "
-        closable
         @close="toggleAlert"
     />
     <div class="card">
       <button class="btn primary" @click="toggleAlert">{{ alert ? 'Скрыть' : 'Показать' }} сообщение</button>
     </div>
-
-    <app-block/>
   </div>
 </template>
 
 <script>
 import AppAlert from "@/components/AppAlert.vue";
-import AppBlock from "@/components/AppBlock.vue";
 import {alertMixin} from "@/alertMixin";
 
 export default {
   mixins: [alertMixin],
   components: {
-    AppAlert, AppBlock
+    AppAlert
   }
 }
 </script>
