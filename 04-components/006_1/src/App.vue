6d42052335b049fa5d24fe13a20d86c8ed44cbec
Anton-Osip <osipchika@gmail.com>
2025-11-06 08:47:24 +0300

    components

--- 04-components/006_1/src/App.vue
+++ 04-components/006_1/src/App.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>
+  <div class = "container pt-1">
+    <div class = "card">
+      <h2>Actual news: {{ now }}</h2>
+      <span>Открыто: {{ openRate }} | Прочитано {{ readRate }}</span>
+    </div>
 
+    <app-news
+        v-for = "item in news"
+        @open-news = "openNews"
+        @read-news = "readNews"
+        @unmark="unmark"
+        :title = "item.title"
+        :id = "item.id"
+        :wasRead = "item.wasRead"
+        :isOpen = "item.isOpen"
+        :key = "item.id">
+    </app-news>
   </div>
 </template>
 
 <script>
+
+import AppNews from "@/AppNews.vue";
+
 export default {
-  name: 'App',
+  data: () => ({
+    now: new Date().toLocaleDateString(),
+    isOpen: false,
+    openRate: 0,
+    readRate: 0,
+    news: [
+      {id: 1, title: 'news1', isOpen: false, wasRead: false},
+      {id: 2, title: 'news2', isOpen: false, wasRead: false},
+      {id: 3, title: 'news3', isOpen: false, wasRead: false}]
+  }),
   components: {
+    "app-news": AppNews
+  },
+  methods: {
+    openNews() {
+      this.openRate++
+    },
+    readNews(id) {
+      const currentNews = this.news.findIndex(i => i.id === id)
+      this.news[currentNews].wasRead = true
+      this.readRate++
+    },
+    unmark(id){
+      const currentNews = this.news.findIndex(i => i.id === id)
+      this.news[currentNews].wasRead = false
+      this.readRate--
+    }
   }
 }
 </script>
---
+++ 04-components/006_1/src/AppNews.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class = "card">
+    <h2>{{ title }}</h2>
+    <app-button
+        @action = "open"
+        >
+      {{isNewsOpen ? 'Закрыть' : 'Открыть'}}
+    </app-button>
+    <app-button
+        v-if = "wasRead"
+        @action = "unmark"
+        color = "danger"
+    >
+      Отметить непрочитанной
+    </app-button>
+    <div
+        v-if = "isNewsOpen">
+      <hr>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aliquid cum ea eos facilis ipsam iste laudantium
+        minus, nemo nisi obcaecati placeat quia quis similique sit
+        temporibus totam ullam vero vitae?
+      </p>
+      <app-button
+          v-if = "!wasRead"
+          @action = "mark"
+          color = "primary"
+      >
+        Прочесть новость
+      </app-button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import AppButton from "@/AppButton.vue";
+
+export default {
+  props: {
+    title: {type: String, required: true},
+    wasRead: {type: Boolean, required: true},
+    id: {type: Number, required: true},
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator(value) {
+        return value === true || value === false
+      }
+    }
+  },
+  emits: {
+    'open-news': null,
+    'read-news'(id) {
+      if (id) {
+        return true
+      }
+      console.warn("Нет параметра айди новости")
+      return false
+    },
+    unmark(id) {
+      if (id) {
+        return true
+      }
+      console.warn("Нет параметра айди новости")
+      return false
+    }
+  },
+
+  data() {
+    return {
+      isNewsOpen: this.isOpen
+    }
+  },
+  methods: {
+    open() {
+      this.isNewsOpen = !this.isNewsOpen
+      if (this.isNewsOpen) {
+        this.$emit('open-news')
+      }
+
+    },
+    mark() {
+      this.isNewsOpen = false
+      this.$emit('read-news', this.id)
+    },
+    unmark() {
+      this.isNewsOpen = true
+      this.$emit('unmark', this.id)
+    }
+  },
+  components: {'app-button': AppButton}
+}
+</script>
